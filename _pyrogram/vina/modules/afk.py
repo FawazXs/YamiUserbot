b5bf609a8c3496a4b9d97e5df28daf0fba5f6a08
levina <82658782+levina-lab@users.noreply.github.com>
2021-06-10 02:16:28 +0700

    Update afk.py

--- _pyrogram/vina/modules/afk.py
+++ _pyrogram/vina/modules/afk.py
@@ -10,13 +10,13 @@ import time
 from pyrogram import filters
 import asyncio
 
-from Zect import app, CMD_HELP
+from vina import app, CMD_HELP
 from config import PREFIX, LOG_CHAT
-from Zect.helpers.pyrohelper import get_arg
-import Zect.database.afkdb as Zect
-from Zect.helpers.pyrohelper import user_afk
-from Zect.modules.alive import get_readable_time
-from Zect.helpers.utils import get_message_type, Types
+from vina.helpers.pyrohelper import get_arg
+import vina.database.afkdb as vina
+from vina.helpers.pyrohelper import user_afk
+from vina.modules.alive import get_readable_time
+from vina.helpers.utils import get_message_type, Types
 
 CMD_HELP.update(
     {
@@ -43,14 +43,14 @@ async def afk(client, message):
         reason = None
     else:
         reason = arg
-    await Zect.set_afk(True, afk_time, reason)
+    await vina.set_afk(True, afk_time, reason)
     await message.edit("**I'm goin' AFK**")
 
 
 @app.on_message(filters.mentioned & ~filters.bot & filters.create(user_afk), group=11)
 async def afk_mentioned(_, message):
     global MENTIONED
-    afk_time, reason = await Zect.afk_stuff()
+    afk_time, reason = await vina.afk_stuff()
     afk_since = get_readable_time(time.time() - afk_time)
     if "-" in str(message.chat.id):
         cid = str(message.chat.id)[4:]
@@ -87,7 +87,7 @@ async def afk_mentioned(_, message):
 
 @app.on_message(filters.create(user_afk) & filters.outgoing)
 async def auto_unafk(_, message):
-    await Zect.set_unafk()
+    await vina.set_unafk()
     unafk_message = await app.send_message(message.chat.id, "**I'm no longer AFK**")
     global MENTIONED
     text = "**Total {} mentioned you**\n".format(len(MENTIONED))
