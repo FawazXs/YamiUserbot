fe81d6ab5a7a2418d4f05211d1e6f9fb437cdff1
levina <82658782+levina-lab@users.noreply.github.com>
2021-06-11 19:54:54 +0700

    Update and rename python.txt to afkdb.py

--- _pyrogram/database/afkdb.py
+++ _pyrogram/database/afkdb.py
@@ -1,4 +1,10 @@
-python
+# copyright (c) 2021 by @levina-lab
+#
+# this file is part of < https://github.com/levina-lab/veezuserbot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/levina-lab/veezuserbot/blob/master/LICENSE >
+#
+# All rights reserved.
 
 from . import cli
 import asyncio
