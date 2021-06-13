7ec199d52ce64c6ca6bab0f76b272738e3c12f39
levina <82658782+levina-lab@users.noreply.github.com>
2021-06-13 22:31:15 +0700

    Create kevin.py

---
+++ _pyrogram/modules/kevin.py
@@ -0,0 +1,106 @@
+from time import sleep
+from telethon import events
+from pyrogram import filters
+from pyrogram.types import Message
+
+from telethon.events import register
+from _pyrogram import app, CMD_HELP
+from config import PREFIX
+from _pyrogram.helpers.utils import get_message_type, Types
+import asyncio
+
+
+CMD_HELP.update(
+    {
+        "animation": """
+**📕 MODULE ANIMATION:**
+`━━━━━━━━━━━━━━━━━`
+`animation` ⇛ ketik .droopy untuk melihat keajaiban.
+`animation` ⇛ ketik .kevincan untuk melihat keajaiban.
+"""
+    }
+)
+
+
+@app.on_message(filters.command("kevincan", PREFIX) & filters.me)
+async def koc(_, e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("perkenalkan.")
+        sleep(1)
+        await e.edit("perkenalkan..")
+        sleep(1)
+        await e.edit("perkenalkan...")
+        sleep(1)
+        await e.edit("😎")
+        sleep(3)
+        await e.edit("namaku kevin")
+        sleep(1)
+        await e.edit("kevin forever")
+        sleep(1)
+        await e.edit("kevin handsome")
+        sleep(1)
+        await e.edit("orang nya baik hati..")
+        sleep(1)
+        await e.edit("tampan..")
+        sleep(1)
+        await e.edit("pemberani")
+        sleep(1)
+        await e.edit("seperti...")
+        sleep(1)
+        await e.edit("🗿")
+        sleep(4)
+        await e.edit("yap benar, seperti dia..")
+        sleep(1)
+        await e.edit("tampan bukan ?...")
+        sleep(1)
+        await e.edit("selain tampan..")
+        sleep(1)
+        await e.edit("dan pemberani..")
+        sleep(1)
+        await e.edit("aku juga cerdas..")
+        sleep(1)
+        await e.edit("siapa?..")
+        sleep(1)
+        await e.edit("ya aku dong..")
+        sleep(1)
+        await e.edit("iya aku..")
+        sleep(1)
+        await e.edit("kevin cool..")
+        sleep(1)
+        await e.edit("satu..")
+        sleep(1)
+        await e.edit("kata...")
+        sleep(1)
+        await e.edit("buat....")
+        sleep(1)
+        await e.edit("lu semua.....")
+        sleep(1)
+        await e.edit("ASUUUU...")
+        sleep(1)
+        await e.edit("dahlah....")
+        sleep(1)
+        await e.edit("bye sj...")
+        sleep(1)
+        await e.edit("ku gabut...")
+        sleep(1)
+        await e.edit("gabut sekali....")
+        sleep(1)
+        await e.edit("👻")
+        sleep(4)
+        await e.edit("misi..")
+        sleep(1)
+        await e.edit("foya...")
+        sleep(1)
+        await e.edit("misi..")
+        sleep(1)
+        await e.edit("foya...")
+        sleep(1)
+        await e.edit("don't play play bosquahh 🥵")
+        sleep(1)
+        await e.edit("🤑")
+        sleep(4)
+        await e.edit("sekian...")
+        sleep(1)
+        await e.edit("terimakasih....")
+        sleep(1)
+        await e.edit("TAMAT 😜")
