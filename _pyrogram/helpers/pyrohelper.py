ac27b05a927b556dc1f8f64d8ac9576ec98f2311
levina <82658782+levina-lab@users.noreply.github.com>
2021-06-14 19:56:46 +0700

    Update pyrohelper.py

--- _pyrogram/helpers/pyrohelper.py
+++ _pyrogram/helpers/pyrohelper.py
@@ -1,6 +1,6 @@
 # copyright 2021 (c) Levina Shavila
 
-from afkdb import get_afk_status
+from database import get_afk_status
 from pyrogram.types import Message, User
 from pyrogram import Client
 import shlex
