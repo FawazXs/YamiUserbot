0c818b118f570e40dee36e65c796655e819770dd
levina <82658782+levina-lab@users.noreply.github.com>
2021-06-14 19:23:13 +0700

    Update pyrohelper.py

--- _pyrogram/helpers/pyrohelper.py
+++ _pyrogram/helpers/pyrohelper.py
@@ -1,5 +1,6 @@
 # copyright 2021 (c) Levina Shavila
 
+from pyrogram.database.afkdb import get_afk_status
 from pyrogram.types import Message, User
 from pyrogram import Client
 import shlex
@@ -30,3 +31,11 @@ def get_args(message):
     except ValueError:
         return message  # Cannot split, let's assume that it's just one long message
     return list(filter(lambda x: len(x) > 0, split))
+
+
+async def user_afk(filter, client: Client, message: Message):
+    check = await get_afk_status()
+    if check:
+        return True
+    else:
+        return False
