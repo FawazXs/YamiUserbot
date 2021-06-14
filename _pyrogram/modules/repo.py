a2e2775bb7ae09a4731d8eaa2b17b2f533e2a2e9
levina <82658782+levina-lab@users.noreply.github.com>
2021-06-14 08:14:07 +0700

    Update repo.py

--- _pyrogram/modules/repo.py
+++ _pyrogram/modules/repo.py
@@ -10,10 +10,11 @@ import asyncio
 
 
 @app.on_message(filters.command("repo", PREFIX) & filters.me)
-async def koc(_, e):
+async def koc(_, e, m):
   if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
     await e.edit("╭✠╼━━━━━━❖━━━━━━━✠╮"
-                 "\n` `[🌸 𝐑𝐄𝐏𝐎 𝐕𝐄𝐄𝐙 𝐔𝐒𝐄𝐑𝐁𝐎𝐓](https://github.com/levina-lab/vinauserbot)` `"
-                 "\n╰✠╼━━━━━━❖━━━━━━━✠╯"
-                 "\n🎖` `[GNU GPL V.3.0](https://github.com/levina-lab/vinauserbot/blob/master/LICENSE)` `")
-    await app.send_message(chat.id, disable_web_page_preview=True)
+    reply_msg = f"\n` `[🌸 𝐑𝐄𝐏𝐎 𝐕𝐄𝐄𝐙 𝐔𝐒𝐄𝐑𝐁𝐎𝐓](https://github.com/levina-lab/vinauserbot)` `"
+    reply_msg = f"\n╰✠╼━━━━━━❖━━━━━━━✠╯"
+    reply_msg = f"\n🎖` `[GNU GPL V.3.0](https://github.com/levina-lab/vinauserbot/blob/master/LICENSE)` `")
+    await m.edit()
+    await app.send_message(m.chat.id, disable_web_page_preview=True)
